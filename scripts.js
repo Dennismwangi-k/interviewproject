02672ccf179e58ad69c973f1644f483aeb1bec48
Dennis Mwangi <denixxdenixx64@gmail.com>
2023-08-07 12:19:25 +0300

    interview project

---
+++ scripts.js
@@ -0,0 +1,95 @@
+const videoHero = document.getElementById("video-hero");
+const videoItems = document.getElementsByClassName("video-item");
+const nextBtn = document.getElementsByClassName("next-btn")[0];
+const videoTags = document.getElementsByTagName("video");
+videoHero.addEventListener("mousedown", handleMouseDown);
+videoHero.addEventListener("mouseup", handleMouseUp);
+
+
+function slideCarousel() {
+    const slide = document.querySelector('.carousel-slide');
+    const firstItem = document.querySelector('.carousel-item');
+  
+
+    slide.appendChild(firstItem.cloneNode(true));
+  
+
+    slide.style.transition = 'transform 1s ease';
+    slide.style.transform = 'translateX(-100%)';
+    setTimeout(() => {
+        slide.removeChild(firstItem);
+        slide.style.transition = 'none';
+        slide.style.transform = 'translateX(0)';
+    }, 1000);
+  }
+  setInterval(slideCarousel, 5000);
+  
+
+  function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+const videosLength = videoItems.length;
+const loopTime = 8000;
+let videoIdx = 0;
+let loadPer = 2;
+let isDragging = false;
+let xPos = 0;
+
+function hideVideos(videoIdx = 0) {
+for (let i = 0; i < videoItems.length; i++) {
+  const rmTime = (videoTags[i].duration - videoTags[i].currentTime) * 1000;
+  if (videoIdx != i) {
+    videoItems[i].style.display = "none";
+    videoTags[i].pause();
+    if (i > 0 && rmTime < loopTime) {
+      videoTags[i].currentTime = 0;
+    }
+  } else {
+    videoItems[i].style.display = "block";
+    videoTags[i].play();
+  }
+}
+}
+
+function loopVideos(offset = 1) {
+let newVideoIndex = (videoIdx + offset) % videosLength;
+videoIdx = Math.max(0, newVideoIndex);
+hideVideos(videoIdx);
+}
+
+function moveVideo(offset = 1) {
+loadPer = 2;
+loopVideos(offset);
+}
+
+hideVideos(videoIdx);
+loopVideos();
+setInterval(() => {
+moveVideo();
+}, loopTime);
+
+
+function handleMouseDown(event) {
+isDragging = true;
+xPos = event.clientX;
+}
+
+function handleMouseUp(event) {
+isDragging = false;
+newXPos = event.clientX;
+const dragDistance = newXPos - xPos;
+if (dragDistance > 10) {
+  moveVideo(-1);
+} else if (dragDistance < -10) {
+  moveVideo();
+}
+}
+function loadDocumet() {
+}
+
